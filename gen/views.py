c85abaf6292ad979b417e86f2d2166c1640e2d29
Abhiajinkya <abhiajinkya@gmail.com>
2020-09-01 00:47:36 +0530

    Final

--- gen/views.py
+++ gen/views.py
@@ -8,18 +8,26 @@ def home(request):
 
 def password(request):
 
-
     characters = list('abcdefghijklmnopqrstuvwxyz')
 
-    length=10
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+    # length=10
+    length=int(request.GET.get('length', "12"))
 
     thepassword=''
     for x in range(length):
         thepassword += random.choice(characters)
-
-
-
     return render(request,'gen/password.html',{'password':thepassword})
+
 
 def about(request):
     return render(request, 'gen/about.html')
+
+def portfolio(request):
+    return render(request, 'gen/portfolio.html')
--- password_gen/urls.py
+++ password_gen/urls.py
@@ -18,9 +18,10 @@ from django.urls import path
 from gen import views
 
 urlpatterns = [
-    path('',views.home),
+    path('',views.home,name='home'),
     # path('password',views.password),
     path('generatedpassword/',views.password, name='password'),
-    path('about',views.about),
+    path('about',views.about, name='about'),
+    path('portfolio',views.portfolio,name='portfolio')
     # path('hithere/', admin.site.urls),
 ]
